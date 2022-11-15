fe1baad8f34c63a53b00a33d9d7b9bed8caebdf0
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-11-16 05:56:04 +0700

    initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+const App: FC = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
