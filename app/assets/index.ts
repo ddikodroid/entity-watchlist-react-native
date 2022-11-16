036aa2e537848109524d263c7043e202a46d1500
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-11-16 14:19:57 +0700

    chore(assets): add icons

---
+++ app/assets/index.ts
@@ -0,0 +1,7 @@
+export const ICONS = {
+  arrowUp: require('./icons/arrow-up-circle.png'),
+  arrowDown: require('./icons/arrow-down-circle.png'),
+  search: require('./icons/magnifying-glass.png'),
+  filter: require('./icons/adjustments-horizontal.png'),
+  notFound: require('./icons/face-frown.png'),
+};
